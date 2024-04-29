a0e99ca1b5f1ae192ca2707fbdb12af06463936e
Stanisław Synowiec <stasyn1410@gmail.com>
2024-04-29 19:53:11 +0200

    style: card style post card

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,36 +1,41 @@
 import { PrismicNextImage } from '@prismicio/next';
 import { PrismicLink, PrismicText } from '@prismicio/react';
 import { RichText } from './RichText';
-import { Content } from '@prismicio/client';
+import { type Content } from '@prismicio/client';
+import { type ReactElement } from 'react';
+import { Card } from '@/components/ui/card';
 
 export const PostCard = ({
 	post,
 }: {
 	post: Content.BlogPostDocument;
-}): JSX.Element => {
+}): ReactElement => {
 	const { data } = post;
 
 	return (
-		<PrismicLink document={post} className="grid grid-cols-2 gap-10">
-			<PrismicNextImage
-				field={data.featured_image}
-				sizes="100vw"
-				className="max-h-60 w-full max-w-sm rounded-xl object-cover"
-			/>
-			<div className="flex flex-col gap-3">
-				<div className="flex flex-col gap-1">
-					<p className="w-min border-b-2 pb-1 text-sm text-slate-700 opacity-75">
-						{new Date(data?.publication_date || '').toLocaleDateString()}
-					</p>
-					<div className="transition-all duration-300 ease-in-out hover:opacity-75">
-						<h2 className="text-xl font-bold">
-							<PrismicText field={data.title} />
-						</h2>
+		<PrismicLink document={post} className="flex grid-cols-2 gap-10">
+			<Card className="flex w-full flex-col p-6">
+				<PrismicNextImage
+					field={data.featured_image}
+					priority={true}
+					sizes="100vw"
+					className="max-h-40 w-full rounded-lg object-cover"
+				/>
+				
+				<div className="flex flex-col gap-3">
+					<div className="flex flex-col gap-1">
+						<p className="w-min border-b-2 pt-2 text-sm text-slate-700 opacity-75">
+							{new Date(data?.publication_date || '').toLocaleDateString()}
+						</p>
+						<div className="transition-all duration-300 ease-in-out hover:opacity-75">
+							<h2 className="text-2xl font-bold">
+								<PrismicText field={data.title} />
+							</h2>
+						</div>
 					</div>
+					<RichText field={data.description} />
 				</div>
-				<RichText field={data.description} />
-			</div>
-			<div className="col-span-2 w-full border-b border-solid border-gray-200" />
+			</Card>
 		</PrismicLink>
 	);
 };
